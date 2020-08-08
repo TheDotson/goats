b6300adede74e9d8e105f31d724829a9e5887283
Michael Dotson <thedotson@icloud.com>
2020-08-08 11:02:09 -0500

    inital porject setup

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Goats</h2>
+        <button className="btn btn-info">
+        <i className="fas fa-torii-gate"></i>Goat Button<i className="fas fa-torii-gate"></i></button>
+      </div>
+    );
+  }
+}
+
+export default App;
